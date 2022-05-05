d717a19ecfd76030bd70d95f5505ddcefe7792f0
kmmaddox22 <kmaddox@risd.edu>
2022-05-05 14:08:50 -0400

    Project 3 cleaned up

---
+++ Project 3/script.js
@@ -0,0 +1,24 @@
+
+   var speed = 2800; 
+    var t = setInterval(function(){
+   var slideSource = document.getElementById('shadow');
+   slideSource.classList.toggle('fade');
+  }, speed);
+
+<<<<<<< HEAD
+  var speed = 2800; 
+  var t = setInterval(function(){
+ var slideSource = document.getElementById('alphabet');
+ slideSource.classList.toggle('switch'); slideSource.classList.toggle('switch2');
+}, speed);
+
+
+=======
+>>>>>>> d8040d7995150c518a473f194dc6f2a2b07d073d
+ // var speed = 2800; 
+//  var t = setInterval(function(){
+ //var slideSource = document.getElementById('looking-for-shade');
+ //slideSource.classList.toggle('fade');
+//}, speed);
+    
+ 
