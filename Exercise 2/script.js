11bfcd89c51863c842b527ec8b1d5e809e07f3d9
kmmaddox22 <kmaddox@risd.edu>
2022-05-17 20:01:07 -0400

    bad stuff

---
+++ Exercise 2/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  
+
+	//standard sliders
+	$('.axis-range').on('input', function(){
+
+		let value = parseInt($(this).val()); //get slider input value
+		// console.log(value);
+
+		const slidertype = $(this).data('type'); //get slider axis
+
+		// console.log("--"+slidertype, value );
+
+		$('#bigletter').css("--"+slidertype, value);
+
+	});
+
+});
