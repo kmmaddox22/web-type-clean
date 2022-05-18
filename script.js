c8e49bfb382490a8dd1bb0532ed03e9940a255bc
kmmaddox22 <kmaddox@risd.edu>
2022-05-17 20:30:41 -0400

    link to exercise

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ $(document).ready(function() {
     });
   
   });
+
+  $(document).ready(function() {
+
+  
+
+	//standard sliders
+	$('.axis-range').on('input', function(){
+
+		let value = parseInt($(this).val()); //get slider input value
+		// console.log(value);
+
+		const slidertype = $(this).data('type'); //get slider axis
+
+		// console.log("--"+slidertype, value );
+
+		$('#bigletter').css("--"+slidertype, value);
+
+	});
+
+});
