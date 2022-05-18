529f1fc8ce33b8ab461c2d8c7be4018b3fd897c5
kmmaddox22 <kmaddox@risd.edu>
2022-05-17 20:20:23 -0400

    darkmode

---
+++ script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+    // Your JavaScript goes here.
+    console.log('hello world');
+   
+    $('#darkmode').click(function(){
+      console.log('button clicked');
+  
+      $('body').toggleClass('darkmode');
+  
+    });
+  
+  });
